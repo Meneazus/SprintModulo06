ef5a6f5d54be783a86dd721ce355ea6a654c7b9a
bmunoz <bmunozdiaz12@gmail.com>
2024-10-04 17:50:23 -0300

    Documentacion JavaDoc package Entity y Service

--- src/main/java/cl/grupo02/sprintFinal/repository/CapacitacionRepository.java
+++ src/main/java/cl/grupo02/sprintFinal/repository/CapacitacionRepository.java
@@ -5,6 +5,13 @@ import org.springframework.stereotype.Repository;
 
 import cl.grupo02.sprintFinal.model.entity.Capacitacion;
 
+/**
+ * Interfaz que define los métodos de acceso a datos para la entidad Capacitacion.
+ * Extiende JpaRepository para proporcionar funcionalidades CRUD.
+ * 
+ * @author Ariel Alfaro, Bastian Muñoz, Bastian Espinosa, Joshua Montt, Nicolas Gajardo
+ * @version 1.0
+ */
 @Repository
 public interface CapacitacionRepository extends JpaRepository<Capacitacion, Integer> {
     // Métodos básicos para utilizar el CRUD de JPA.
